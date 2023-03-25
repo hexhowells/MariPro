b688eeb2744a25e762fd4f693e0f834dc78dfd22
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-03-25 10:39:59 +0000

    Add neural network class

---
+++ NeuroEvolution/network.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn
+
+
+class Layer(nn.Module):
+	def __init__(self, in_size, out_size):
+		super().__init__()
+
+		self.layer = nn.Linear(in_size, out_size)
+		self.relu = nn.ReLU(inplace=True)
+
+
+	def forward(self, x):
+		x = self.layer(x)
+		x = self.relu(x)
+
+		return x
+
+
+class Model(nn.Module):
+	def __init__(self, dims):
+		super().__init__()
+
+		layers = [Layer(d1, d2) for d1, d2 in zip(dims, dims[1:])]
+		self.layers = nn.Sequential(*layers)
+
+
+	def forward(self, x):
+		x = x.flatten()
+		x = self.layers(x)
+
+		return x
+
+
+class SimpleModel(nn.Module):
+	def __init__(self, in_size, out_size):
+		super().__init__()
+
+		self.layer = nn.Linear(in_size, out_size)
+		self.relu = nn.ReLU(inplace=True)
+
+	def forward(self, x):
+		x = x.flatten()
+		x = self.layer(x)
+		x = self.relu(x)
+
+		return x
