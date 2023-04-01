1897278e44b33fbc4637722c9c82ca52e6f6a38d
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-04-01 18:44:11 +0100

    add random mutation function
    completely changes weight instead of nudging it. Results in quite poor performance

--- NeuroEvolution/mutation.py
+++ NeuroEvolution/mutation.py
@@ -5,7 +5,7 @@ import torch
 
 
 def uniform_mutation(evo, chromosomes, fitness_scores):
-    """ Uniform mutation - mutates random genes uniformly
+    """ Uniform mutation - modifies random genes by small amounts
 
         Args:
             evo (Evolution): Evolution class instance
@@ -26,6 +26,33 @@ def uniform_mutation(evo, chromosomes, fitness_scores):
             # Mutate the selected weights.
             with torch.no_grad():
                 param.flatten()[indices] += torch.randn(num_to_mutate) * 0.01
+
+        evo.population.append(chromosome)
+        evo.fitness_scores.append(0)
+
+
+def random_mutation(evo, chromosomes, fitness_scores):
+    """ Random mutation - changes random genes uniformly
+
+        Args:
+            evo (Evolution): Evolution class instance
+            chromosomes (list): chromosomes to mutate
+            fitness_scores (list): fitness scores of the chromosomes
+    """
+    for chromosome in chromosomes:
+        for param in chromosome.parameters():
+            # Get the number of weights in the linear layer.
+            num_weights = param.numel()
+
+            # Compute the number of weights to mutate.
+            num_to_mutate = int(num_weights * evo.mutation_rate)
+
+            # Get the indices of the weights to mutate.
+            indices = random.sample(range(num_weights), num_to_mutate)
+
+            # Mutate the selected weights.
+            with torch.no_grad():
+                param.flatten()[indices] = torch.randn(num_to_mutate) * 0.1
 
         evo.population.append(chromosome)
         evo.fitness_scores.append(0)
