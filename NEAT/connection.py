7f7d48aff40e5e231892f1ebdf66711851571c62
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-04-26 21:37:46 +0100

    Split classes into files
    Split genome, node, connection into separate files for better organisation

---
+++ NEAT/connection.py
@@ -0,0 +1,19 @@
+class ConnectGene:
+	""" Class to represent the Connection Gene
+
+		Args:
+			in_node (int): index for the input node
+			out_node (int): index for the output node
+			innovatinon (int): the innovation number
+			weight (float): the connection weight
+			enabled (boolean): if the connection is enabled
+	"""
+	def __init__(self, in_node, out_node, innovation, weight=None, enabled=True):
+		self.in_node = in_node
+		self.out_node = out_node
+		self.innovation = innovation
+		self.weight = weight if weight else random.uniform(-1, 1)
+		self.enabled = enabled
+
+	def __str__(self):
+		return f'ConnectGene {self.in_node} -> {self.out_node}\tInnovation: {self.innovation}\tenabled={self.enabled}'
---
+++ NEAT/node.py
@@ -0,0 +1,20 @@
+class NodeGene:
+	""" Class to represent the Node Gene
+
+		Args:
+			node_idx (int): index of the node
+			node_type (string): node type
+			ref (int): reference index to input or output array
+	"""
+	def __init__(self, node_idx, node_type, ref=None):
+		self.idx = node_idx
+		self.type = node_type
+		self.ref = ref
+		self.connections = []
+
+	def add_connection(self, connection):
+		self.connections.append(connection)
+
+	def __str__(self):
+		ref_str = "Ref: "+str(self.ref) if self.ref is not None else ""
+		return f'NodeGene {self.idx}\tType: {self.type}\t{ref_str}'
