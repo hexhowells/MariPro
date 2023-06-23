753a4f781d91cc73a1bec27eb0de2af6f2b2ec25
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-06-23 23:07:19 +0100

    remove fitness scores from crossover function
    are not needed since fitness scores are stored in genome objects

--- NEAT/crossover.py
+++ NEAT/crossover.py
@@ -1,12 +1,12 @@
 from genome import Genome
 import random
 
-def crossover(parent1, parent2, fitness1, fitness2):
+def crossover(parent1, parent2):
 	matching_genes = parent1.get_matching_genes(parent2)
 
 	connect_genes = [random.choice([c1, c2]) for (c1, c2) in matching_genes]
 
-	if fitness1 > fitness2:
+	if parent1.fitness > parent2.fitness:
 		excess_genes, disjoint_genes = parent1.get_non_matching_genes(parent2)
 		max_innov = parent1.innovation
 		node_len = len(parent1.node_genes)
