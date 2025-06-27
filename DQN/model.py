822e993779be90b0d929f62aa2b855e94bc971ff
hexhowells <morganhowells314@gmail.com>
2025-06-27 18:23:37 +0100

    update model

--- DQN/model.py
+++ DQN/model.py
@@ -7,19 +7,18 @@ class QNetwork(nn.Module):
 		super().__init__()
 
 		self.features = nn.Sequential(
-			nn.Conv2d(in_channels, 32, kernel_size=(3,3), stride=2, padding=1),
+			nn.Conv2d(in_channels, 32, kernel_size=(8,8), stride=4),
 			nn.ReLU(inplace=True),
-			nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1),
+			nn.Conv2d(32, 64, kernel_size=(4,4), stride=2),
 			nn.ReLU(inplace=True),
-			nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1),
-			nn.ReLU(inplace=True),
-			nn.Conv2d(32, 32, kernel_size=(3,3), stride=2, padding=1),
+			nn.Conv2d(64, 64, kernel_size=(3,3), stride=1),
 			nn.ReLU(inplace=True),
 			)
 		self.fc = nn.Sequential(
 			nn.Flatten(),
-			nn.Linear(512, 100),
-			nn.Linear(100, actions),
+			nn.Linear(3136, 512),
+			nn.ReLU(inplace=True),
+			nn.Linear(512, actions),
 			)
 
 
