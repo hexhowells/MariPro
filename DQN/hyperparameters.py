9492c8bcd9cb6953cba8851a6f1a54032fd5a9da
hexhowells <morganhowells314@gmail.com>
2025-06-28 15:15:37 +0100

    update model and hyperparams

--- DQN/hyperparameters.py
+++ DQN/hyperparameters.py
@@ -1,20 +1,22 @@
-batch_size = 32
+batch_size = 64
 lr = 3e-4
 
 history_len = 4
 frame_size = 84
-replay_memory_size = 10_000
+in_channels = 4
+action_space = 7
+replay_memory_size = 100_000
 
-target_update_frequency = 10_000
-update_frequency = 4
+target_update_frequency = 5_000
+update_frequency = 2
 
 epsilon = 1.0
 epsilon_min = 0.01
-epsilon_decay = 0.995
+epsilon_decay = 0.999
 discount_factor = 0.99
 action_repeats = 4
 
-replay_start_size = 5000 # random policy is run for this many frames before training
+reply_start_size = 50_000 # random policy is run for this many frames before training
 
-num_episodes = 10_000
+num_episodes = 100_000
 episode_len = 20_000
