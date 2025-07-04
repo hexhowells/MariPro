3afd5d72e32c2c2c5edb8f12440fb8e522583983
hexhowells <morganhowells314@gmail.com>
2025-07-04 20:10:37 +0100

    update reward calculation and tracking

--- DQN/env.py
+++ DQN/env.py
@@ -9,53 +9,54 @@ class Environment:
 		self.x_history = deque([-1], maxlen=60)
 		self.high_score = 0
 		self.total_reward = 0
-		self.done = False
+		self.curr_lives = 2
 
 
 	def reset(self):
 		self.x_history = deque([-1], maxlen=60)
 		self.high_score = 0
 		self.total_reward = 0
-		self.done = False
 
 		return self.env.reset()
 
 
 	def _step(self, action):
 		state, reward, terminated, truncated, info = self.env.step(action)
-		self._update_high_score(info['x_pos'])
+		self.high_score = max(self.high_socre, info['x_pos'])
 		self.x_history.append(info['x_pos'])
+
+		done = terminated or truncated or (info['life'] < self.curr_lives) or self._not_moved()
+
+		self.curr_lives = max(self.curr_lives, info['life'])
 		
-		return state, reward, terminated, truncated, info
+		return state, reward, done, info
 
 
 	def _not_moved(self):
 		return len(set(self.x_history)) <= 1
 
 
-	def _update_high_score(self, x_pos):
-		if x_pos > self.high_score:
-			self.high_score = x_pos
-
-
 	def step(self, action, n=4):
 		reward_history = []
+		done = False
 
 		for _ in range(n):
 			
-			next_frame, reward, terminated, truncated, _ = self._step(action)
+			next_frame, reward, done, _ = self._step(action)
 			reward_history.append(reward)
 
-			if terminated or truncated or self._not_moved():
-				self.done = True
+			if done:
+				done = True
 				break
 
+		reward_history = np.clip(reward_history, -1, 1)
+
 		avg_reward = sum(reward_history) / len(reward_history)
-		avg_reward = np.clip(avg_reward, -1, 1)
+		self.total_reward = avg_reward
 
-		self.total_reward += avg_reward
+		if done: avg_reward = -1
 
-		return next_frame, avg_reward, self.done
+		return next_frame, avg_reward, done
 
 
 class Breakout:
